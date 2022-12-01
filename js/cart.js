e7b5c858f06b48c8634d5fe0babc21a78cb9a7a5
Maksim Leskin <maksim.leskin@methed.ru>
2022-12-01 18:59:08 +0300

    ready

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,7 @@
-import { catalogList, countAmount, modalProductBtn, orderCount, orderList } from './elements.js';
+import { catalogList, countAmount, modalDelivery, modalProductBtn, order, orderCount, orderList, orderSubmit, orderTotalAmount, orderWrapTitle } from './elements.js';
 import { getData } from './getData.js';
 import { API_URL, PREFIX_PRODUCT } from './const.js';
+import { orderController } from './orderController.js';
 
 const getCart = () => {
   const cartList = localStorage.getItem('cart');
@@ -13,8 +14,12 @@ const getCart = () => {
 
 const renderCartList = async () => {
   const cartList = getCart();
+  
+  orderSubmit.disabled = !cartList.length;
   const allIdProduct = cartList.map(item => item.id)
-  const data = await getData(`${API_URL}${PREFIX_PRODUCT}?list=${allIdProduct}`)
+  const data = cartList.length
+    ? await getData(`${API_URL}${PREFIX_PRODUCT}?list=${allIdProduct}`)
+    : [];
   
   const countProduct = cartList.reduce((acc, item) => acc + item.count, 0);
   orderCount.textContent = countProduct;
@@ -39,11 +44,11 @@ const renderCartList = async () => {
       </div>
 
       <div class="order__product-count count">
-        <button class="count__minus">-</button>
+        <button class="count__minus" data-id-product=${product.id}>-</button>
 
         <p class="count__amount">${product.count}</p>
 
-        <button class="count__plus">+</button>
+        <button class="count__plus" data-id-product=${product.id}>+</button>
       </div>
     `;
 
@@ -52,6 +57,11 @@ const renderCartList = async () => {
 
   orderList.textContent = '';
   orderList.append(...cartItems);
+
+  orderTotalAmount.textContent = data.reduce((acc, item) => {
+    const product = cartList.find((cartItem => cartItem.id === item.id));
+    return acc + (item.price * product.count);
+  }, 0);
 };
 
 const updateCartList = (cartList) => {
@@ -73,7 +83,15 @@ const addCart = (id, count = 1) => {
 };
 
 const removeCart = (id) => {
+  const cartList = getCart();
+  const productIndex = cartList.findIndex((item) => item.id === id);
+  cartList[productIndex].count -= 1;
 
+  if (cartList[productIndex].count === 0) {
+    cartList.splice(productIndex, 1)
+  }
+
+  updateCartList(cartList);
 }
 
 const cartController = () => {
@@ -88,6 +106,33 @@ const cartController = () => {
       modalProductBtn.dataset.idProduct,
       parseInt(countAmount.textContent),
     )
+  });
+
+  orderList.addEventListener('click', ({target}) => {
+    const targetPlus = target.closest('.count__plus');
+    const targetMinus = target.closest('.count__minus');
+
+    if (targetPlus) {
+      addCart(targetPlus.dataset.idProduct);
+    }
+
+    if (targetMinus) {
+      removeCart(targetMinus.dataset.idProduct);
+    }
+  });
+
+  orderWrapTitle.addEventListener('click', () => {
+    order.classList.toggle('order_open');
+  });
+
+  orderSubmit.addEventListener('click', () => {
+    modalDelivery.classList.add('modal_open');
+  });
+
+  modalDelivery.addEventListener('click', ({target}) => {
+    if (target.closest('.modal__close') || modalDelivery === target) {
+      modalDelivery.classList.remove('modal_open')
+    }
   })
 };
 
@@ -96,4 +141,5 @@ const cartController = () => {
 export const cartInit = () => {
   cartController();
   renderCartList();
+  orderController(getCart);
 }
